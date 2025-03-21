fd1b2d0c743da690b333d48aa69de6facde536aa
DESKTOP-DL3TM1L\ADMIN <nicope2004@gmail.com>
2025-03-21 15:45:57 -0500

    Ajuste de estructura y estilos de formularios

--- src/app/modules/marketing/marketing-routing.module.ts
+++ src/app/modules/marketing/marketing-routing.module.ts
@@ -1,9 +1,9 @@
 import { RouterModule, Routes } from "@angular/router";
 import { MarketingComponent } from "./pages/marketing/marketing.component";
 import { NgModule } from "@angular/core";
-import { MensajesMasivosComponent } from "./components/mensajes-masivos/mensajes-masivos.component";
-import { CreateMessagesComponent } from "./components/mass-messages/create-messages.component";
-import { MessageHistoryComponent } from "./components/message-history/message-history.component";
+import { MensajesMasivosComponent } from "./pages/mensajes-masivos/mensajes-masivos.component";
+import { CreateMessagesComponent } from "./pages/mass-messages/create-messages.component";
+import { MessageHistoryComponent } from "./pages/message-history/message-history.component";
 
 const routes: Routes = [
   { path: '', component: MarketingComponent },
